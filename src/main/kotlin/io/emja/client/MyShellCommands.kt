23d4190b28f46de27ec4d8d4a1c139fa52b64cce
Helmut Pasch <helmut.pasch@koerber.com>
2024-08-15 11:25:05 +0200

    Implement a first draft of metric sampling ...

--- src/main/kotlin/io/emja/client/MyShellCommands.kt
+++ src/main/kotlin/io/emja/client/MyShellCommands.kt
@@ -1,13 +1,21 @@
 package io.emja.client
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
-import kotlinx.coroutines.*
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.system.measureTimeMillis
 
 @ShellComponent
 class MyShellCommands {
+
+
+    // TODO: Reex matched nicht den value
+    val valueExtractor = Regex(".*\"value\":(.*?)}")
 
     @ShellMethod("Say hello to someone")
     fun sayHello(name: String): String {
@@ -16,6 +24,40 @@ class MyShellCommands {
 
     @ShellMethod("Start Performance Test")
     fun startPerformanceTest(url: String, count: Int): String {
+        val result = runBlocking {
+            measureTimeMillis {
+                val metricResult = async {
+                    startMetricScrapping()
+                }
+                val loadResult = async {
+                    startTestLoad(url, count)
+                }
+                runBlocking {
+                    println("${metricResult.await()} \n${loadResult.await()}")
+                }
+            }
+        }
+        return "Finished time taken: $result ms"
+    }
+
+    suspend fun startMetricScrapping(): String {
+
+        // TODO: Lauf in einer Schleife mit delay 1 sec und sammel die werte in  einer Liste
+        return " ${getMetric("process.cpu.time")}"
+    }
+
+    fun getMetric(name: String): String? {
+        val baseUrl = "http://localhost:8080/actuator/metrics"
+        val url = "$baseUrl/$name"
+        val connection = URL(url).openConnection() as HttpURLConnection
+        val metricJson = connection.inputStream.bufferedReader().use { it.readText() }
+
+        println(valueExtractor.matchEntire(metricJson)?.groups)
+
+        return valueExtractor.matchEntire(metricJson)?.groups?.get(1)?.value
+    }
+
+    suspend fun startTestLoad(url: String, count: Int): String {
         val start = System.currentTimeMillis()
         val latency = parallelGetRequests(url, count)
         val end = System.currentTimeMillis()
